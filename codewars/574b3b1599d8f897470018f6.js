4b8babfb0077aeba0327b9c5b449b2c1428051af
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-07 17:50:35 +0300

    feat: added the codewars solution 574b3b1599d8f897470018f6

---
+++ codewars/574b3b1599d8f897470018f6.js
@@ -0,0 +1,16 @@
+/**
+ * @difficulty 8 kyu
+ * @description This file contains an answer for <What's the real floor?>
+
+> codewars task
+ * @link https://www.codewars.com/kata/574b3b1599d8f897470018f6
+ * @author Komarovsky Andrey (askomar)
+ *
+ * Task short description:
+ *  Write aa fucntion that given a floor in the american system returns the 
+ * floor in the european system
+ */
+function getRealFloor(n) {
+  return n >= 1 && n >= 13 ? n - 2 : n >= 1 ? n - 1 : n == 0 ? 0 : n;
+}
+
