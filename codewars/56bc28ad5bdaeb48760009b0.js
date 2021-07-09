1e0d92af065f9b6c53e1e5e7dd304d22d31ef3c9
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-09 20:59:15 +0300

    feat: added the codewars solution 56bc28ad5bdaeb48760009b0

---
+++ codewars/56bc28ad5bdaeb48760009b0.js
@@ -0,0 +1,10 @@
+/**
+ * @difficulty 8 kyu
+ * @description This file contains an answer for <Remove First and Last Character> codewars task
+ * @link https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+ * @author Komarovsky Andrey (askomar)
+ */
+
+function removeChar(str) {
+  return str.substring(1, str.length - 1);
+}
