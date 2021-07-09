1522ba5939cd205f8dd3ca6af3f4a16ff425cd2a
Your Name <sashakorol584@mail.ru>
2021-07-10 01:50:49 +0300

    solution

---
+++ validParentheses.js
@@ -0,0 +1,15 @@
+function validParentheses(parens){
+  let count = 0;
+  for (let i = 0; i < parens.length; ++i) {
+    if (parens[i] == '(') {
+      count++;
+      }
+    if (parens[i] == ')') {
+      count--;
+      }
+    if (count < 0) {
+      return false;
+      }
+  }
+  return count == 0;
+}
