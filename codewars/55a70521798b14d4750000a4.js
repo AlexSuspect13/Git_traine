90d2b273260dfd8fff428c8216072b7f8bb8bde8
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-09 19:54:02 +0300

    feat: added the codewars solution 55a70521798b14d4750000a4

---
+++ codewars/55a70521798b14d4750000a4.js
@@ -0,0 +1,10 @@
+/**
+ * @difficulty 8 kyu
+ * @description This file contains an answer for <Returning Strings> codewars task
+ * @link https://www.codewars.com/kata/55a70521798b14d4750000a4
+ * @author Komarovsky Andrey (askomar)
+ */
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
