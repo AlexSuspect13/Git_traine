d622519924a96553e16f2f480adfc21081af52f9
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-09 20:03:13 +0300

    feat: added the codewars ssolution 55685cd7ad70877c23000102

---
+++ codewars/55685cd7ad70877c23000102.js
@@ -0,0 +1,10 @@
+/**
+ * @difficulty 8 kyu
+ * @description This file contains an answer for <Return Negative> codewars task
+ * @link https://www.codewars.com/kata/55685cd7ad70877c23000102
+ * @author Komarovsky Andrey (askomar)
+ */
+
+function makeNegative(num) {
+  return num < 0 ? num : -1 * num;
+}
