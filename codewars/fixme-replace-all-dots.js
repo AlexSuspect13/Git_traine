36d3c2e9dab840b810cb38b622cc4362a571d1ee
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-10 13:18:27 +0300

    refactor: added global replace, changed var to const

--- codewars/fixme-replace-all-dots.js
+++ codewars/fixme-replace-all-dots.js
@@ -1,10 +1,5 @@
 //link: https://www.codewars.com/kata/596c6eb85b0f515834000049
 
-var replaceDots = function (str) {
-    let strCopy = str;
-    while (strCopy.includes(".")) {
-      strCopy = strCopy.replace(/\./, "-");
-    }
-    return strCopy;
-  };
-  
+const replaceDots = function (str) {
+  return str.replace(/\./g, "-");
+};
