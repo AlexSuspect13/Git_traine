9166f1ca18139726fdeeafd4d4f92f1e0bec121e
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-10 13:56:07 +0300

    refactor: splitted code into one line

--- codewars/square-n-sum.js
+++ codewars/square-n-sum.js
@@ -1,7 +1,5 @@
 // https://www.codewars.com/kata/515e271a311df0350d00000f
 
-const reducer = (accumulator, currentValue) => accumulator + Math.pow(currentValue, 2);
+  const squareSum = numbers => numbers.reduce((acum, el) => acum + Math.pow(el, 2));
 
-function squareSum(numbers) {
-    return numbers.reduce(reducer);
-  }
+  
