40a1b86288f0663b68127676977d2c329e09fb4b
TADY <alexqazpl@gmail.com>
2021-07-10 21:57:09 +0300

    my solution

---
+++ waveSorting.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+void wave_sort(std::vector<int> &v) 
+{
+  sort(v.begin(), v.end());
+  size_t len;
+  if ((len = v.size()) > 1)
+    for (size_t i = 0; i < v.size()-1; i += 2)
+      std::swap(v[i], v[i+1]);
+}
