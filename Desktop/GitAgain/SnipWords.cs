5b897c859b03c8922d1ecb6318bf1c41c2e120fd
Dima <dmitry.voroniuk@gmail.com>
2021-07-11 21:47:29 +0300

    Push Solution

---
+++ Desktop/GitAgain/SnipWords.cs
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+  public static string SpinWords(string sentence)
+  {
+    return String.Join(" ", sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
+  }
+}
