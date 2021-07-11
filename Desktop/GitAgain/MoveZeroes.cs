93adf844184d11160885c86881b26f6ac3f02c89
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-12 02:15:14 +0300

    refactor: deleted unused files

--- Desktop/GitAgain/MoveZeroes.cs
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/52597aa56021e91c93000cb0
-using System;
-using System.Collections.Generic;
-using System.Linq;
-public class Kata
-{
-  public static int[] MoveZeroes(int[] arr) => arr.OrderBy(x => x == 0).ToArray();
-}
