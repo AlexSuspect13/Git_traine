bc2d93fe7e7bcf013d4aecbe80e13d6006d670c1
Dima <dmitry.voroniuk@gmail.com>
2021-07-11 21:40:53 +0300

    Peplace mistake

--- Desktop/GitAgain/Persistence.cs
+++ Desktop/GitAgain/Persistence.cs
@@ -1,28 +1,28 @@
 // https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
 using System;
 
-public class Persist 
+public class Persist
 {
-  public static int Persistence(long n) 
-  {
-     int counter = 0;
-            while(n > 0)
+    public static int Persistence(long n)
+    {
+        int counter = 0;
+        while (n > 0)
+        {
+            var count = (int)Math.Ceiling(Math.Log10(Math.Abs(n) + 0.5));
+            if (n < 10)
+                return counter;
+            var loopcount = count;
+            long[] array = new long[count];
+            long recursiacount = 1;
+            for (int i = 0; i < count; i++)
             {
-                var count = (int)Math.Ceiling(Math.Log10(Math.Abs(n) + 0.5));
-                if (n < 10)
-                    return counter;
-                var loopcount = count;
-                long[] zxc = new long[count];
-                long recursia = 1;
-                for (int i = 0; i < count; i++)
-                {
-                    zxc[i] = n / (long)Math.Pow(10, loopcount - 1) % 10;
-                    loopcount--;
-                    recursia *= zxc[i];
-                }
-                counter++;
-                n = recursia;
+                array[i] = n / (long)Math.Pow(10, loopcount - 1) % 10;
+                loopcount--;
+                recursiacount *= array[i];
             }
-            return counter;
-  }
+            counter++;
+            n = recursiacount;
+        }
+        return counter;
+    }
 }
