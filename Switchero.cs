29a6ea1eeaf2188dc119c75f3551bbe780d95468
Dima <dmitry.voroniuk@gmail.com>
2021-07-12 19:59:38 +0300

    Solution

---
+++ Switchero.cs
@@ -0,0 +1,8 @@
+//  https://www.codewars.com/kata/57f759bb664021a30300007d
+public class Kata
+{
+  public static string Switcheroo(string x)
+  {
+     return x.Replace("a","d").Replace("b","a").Replace("d","b");
+  }
+}
