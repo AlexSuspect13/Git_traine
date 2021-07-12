1ff2ee130f704e4ada52e72e429cc43cc30eb0f1
Dima <dmitry.voroniuk@gmail.com>
2021-07-12 20:30:50 +0300

    Fizz buzz c# 8kyu

---
+++ FizzBuzz.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/569e09850a8e371ab200000b
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Kata
+{
+  public static int[] PreFizz(int n) => Enumerable.Range(1, n).ToArray();
+}
