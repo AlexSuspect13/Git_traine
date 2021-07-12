352fa1e794902d3d39e3469f4a15a2257b6b157f
Dima <dmitry.voroniuk@gmail.com>
2021-07-12 23:29:45 +0300

    createVector c++ 8 kyu

---
+++ createVector.cpp
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/55c28f7304e3eaebef0000da
+std::vector<int> createVector(const int n)
+{
+  std::vector<int> res;
+
+  for (int i = 1; i <= n;++i)
+  {
+    res.push_back(i);
+  }
+
+  return res;
+}
