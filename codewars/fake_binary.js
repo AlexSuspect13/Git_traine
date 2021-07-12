29c632a59474f82a6d5ee76a02c6c27677413c2d
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-12 23:35:08 +0300

    feat: added solution for the <Fake BInary> codewars task

---
+++ codewars/fake_binary.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57eae65a4321032ce000002d
+
+function fakeBin(x) {
+  return x
+    .split("")
+    .map((el) => {
+      return el < 5 ? "0" : "1";
+    })
+    .join("");
+}
