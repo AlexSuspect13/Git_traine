49f41bf7793894fe16f4affe8bf1240da6e52109
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-12 13:03:55 +0300

    feat: add solution for the <Remove exclamation marks> codewars task

---
+++ codewars/remove_exclamation_marks.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+function removeExclamationMarks(s) {
+  return s
+    .split("")
+    .map((mark) => {
+      return mark === "!" ? "" : mark;
+    })
+    .join("");
+}
