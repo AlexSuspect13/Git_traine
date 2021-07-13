bb20584775c080a8e591afb286c344770c36eafd
Dima <dmitry.voroniuk@gmail.com>
2021-07-13 21:32:15 +0300

    GetPlanetName\ 8 kyu c#

---
+++ GetPlanetName.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+public class Kata
+{
+  public static string GetPlanetName(int id)
+  {
+    string[] planets = new string[] {"","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"};
+    return planets[id];
+  }
+}
