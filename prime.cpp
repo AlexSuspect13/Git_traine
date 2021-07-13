10fe4781ac7437fe0778b22eb115400afed43c18
KarinaKlevzhits <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-07-13 19:23:32 +0300

    solution

---
+++ prime.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+bool isPrime(int num) {
+   for(int i =2;i<=sqrt(num);++i)
+     {
+     if(num%i==0)
+       {
+       return false;
+     }
+   }
+  return num<=1?false:true;
+}
