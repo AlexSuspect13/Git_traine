92eeaa752dfdd900b3992b1c22b19e95e4b8df6e
TADY <alexqazpl@gmail.com>
2021-07-14 00:07:51 +0300

    my solution

---
+++ dontGiveMeFive.cpp
@@ -0,0 +1,14 @@
+bool containsFive(int num) {
+  for (; num > 0; num /= 10) {
+    if (num%10 == 5) { return true; }
+  }
+  return false;
+}
+
+int dontGiveMeFive(int start, int end) {
+  int count = 0;
+  for (int i = start; i <= end; ++i) {
+    if (!containsFive(abs(i))) { count++; }
+  }
+  return count;
+}
