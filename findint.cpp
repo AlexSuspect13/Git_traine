86a1cc2ef64452f3d6f0aa7c74e6f0640e820a42
KarinaKlevzhits <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-07-13 12:33:24 +0300

    solution

---
+++ findint.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+int findOdd(const std::vector<int>& numbers){
+  for(int i =0;i<numbers.size();++i)
+    {
+    if(std::count(numbers.begin(),numbers.end(),numbers[i])%2!=0)
+      {
+      return numbers[i];
+    }
+  
+  }
+ 
+}
