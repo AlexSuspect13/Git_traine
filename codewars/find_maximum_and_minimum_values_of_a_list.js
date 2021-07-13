39795c9b06f3265905f068755edb25af87dc7066
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-13 12:44:32 +0300

    feat: added solution for the <Find Maximum and Minimum Values of a List> codewars task

---
+++ codewars/find_maximum_and_minimum_values_of_a_list.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
+const min = function (list) {
+  return Math.min(...list);
+};
+
+const max = function (list) {
+  return Math.max(...list);
+};
