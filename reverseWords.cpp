e252021b3eaf72743eea33a2e8a9a377a8892c8e
TADY <alexqazpl@gmail.com>
2021-07-14 00:05:28 +0300

    my solution

---
+++ reverseWords.cpp
@@ -0,0 +1,11 @@
+std::string reverse_words(std::string str){
+  auto b = str.begin(); 
+  auto e = str.end(); 
+  
+  while(b < e){
+    auto k = std::find(b,e,' '); 
+    std::reverse(b,k); 
+    b = k + 1; 
+  }
+  return str; 
+}
