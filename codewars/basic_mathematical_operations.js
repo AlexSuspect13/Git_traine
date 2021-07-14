879e290489d723ce991d36741fef4cbfba24f229
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-14 20:15:46 +0300

    refactor: deleted temp variable for returning

--- codewars/basic_mathematical_operations.js
+++ codewars/basic_mathematical_operations.js
@@ -1,23 +1,20 @@
 // https://www.codewars.com/kata/57356c55867b9b7a60000bd7
 
 function basicOp(operation, value1, value2) {
-  let result;
-
   switch (operation) {
     case "+":
-      result = value1 + value2;
+      return value1 + value2;
       break;
     case "-":
-      result = value1 - value2;
+      return value1 - value2;
       break;
     case "*":
-      result = value1 * value2;
+      return value1 * value2;
       break;
     case "/":
-      result = value1 / value2;
+      return value1 / value2;
       break;
     default:
-      result = 0;
+      return 0;
   }
-  return result;
 }
