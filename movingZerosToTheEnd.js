b5ba8ae6e8bd9b69f0cd391ba4edd3c021c36a5c
Your Name <sashakorol584@mail.ru>
2021-07-14 13:45:56 +0300

    newSolution

--- movingZerosToTheEnd.js
+++ movingZerosToTheEnd.js
@@ -1,15 +1,11 @@
 let moveZeros = function (arr) {
   const arr1 = [];
   const arr2 = [];
-  let result;
-  for (let i = 0; i < arr.length; i++) {
-  if (arr[i] ===  0) {
-    arr1.push(arr[i]);
-  } 
-    else {
-   arr2.push(arr[i]);
+  for (let i in arr) {
+  if (arr[i] === 0)
+    arr1.push(arr[i])
+  else
+    arr2.push(arr[i])
   }
-  }
-  result = arr2.concat(arr1);
-  return result;
+  return arr2.concat(arr1)
 }
