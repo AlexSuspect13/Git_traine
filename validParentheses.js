5f2acfae28dd214b39b65972c4f1886db2777920
Your Name <sashakorol584@mail.ru>
2021-07-14 13:51:35 +0300

    newSolution

--- validParentheses.js
+++ validParentheses.js
@@ -1,15 +1,12 @@
 function validParentheses(parens){
-  let count = 0;
-  for (let i = 0; i < parens.length; ++i) {
-    if (parens[i] == '(') {
-      count++;
-      }
-    if (parens[i] == ')') {
-      count--;
-      }
-    if (count < 0) {
-      return false;
-      }
+  const count = 0;
+  for (let i in parens) {
+    if (parens[i] == '(')
+      count++
+    if (parens[i] == ')')
+      count--
+    if (count < 0)
+      return false
   }
   return count == 0;
 }
