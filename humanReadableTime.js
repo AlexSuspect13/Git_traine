d3a1cd21b71ad839ab70eaceb989635339d61798
Your Name <sashakorol584@mail.ru>
2021-07-14 13:16:23 +0300

    newSolution

--- humanReadableTime.js
+++ humanReadableTime.js
@@ -1,12 +1,13 @@
 function humanReadable(seconds){
-  let hh = seconds / 3600;
-  let mm = (seconds / 60) % 60;
-  let ss = seconds % 60;
+  const hh = Math.floor(seconds / 3600)
+  const mm = Math.floor((seconds / 60) % 60)
+  const ss = Math.floor(seconds % 60)
   if(seconds >= 0 && seconds <= 359999){
-      return time(hh) +':' + time(mm)+':'+time(ss);
+      return (`${time(hh)}:${time(mm)}:${time(ss)}`)
   }
 }
 function time(digit){
-  let num = Number.parseInt(digit)
-  return num > 9 ? num : '0' + num;
+  const num = +(digit)
+  return num > 9 ? num : (`0${num}`)
 }
+
