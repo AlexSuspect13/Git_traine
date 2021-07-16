66ee18527705a6a405b1c707c9943018e1499bdf
KarinaKlevzhits <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-07-16 22:49:54 +0300

    NewFindPosition

---
+++ position.js
@@ -0,0 +1,5 @@
+function position(alphabet){
+  let letters = [" ", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let placed = letters.indexOf(alphabet);
+  return "Position of alphabet:" + " " + placed;
+}
