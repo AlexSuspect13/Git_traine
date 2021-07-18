3f4947c6c6488567205e0e04e9fa6734b53a7b6f
Mihail.Gorelikov <Mihail.Gorelikov@itechart-group.com>
2021-07-18 11:37:33 +0300

    add test

--- main.test.js
+++ main.test.js
@@ -5,6 +5,10 @@ describe('main success', () => {
         expect(sum([1, 2])).toBe(3)
     })
 
+    it('should return -1', () => {
+        expect(sum([1, -2])).toBe(-1)
+    })
+
     it('should return 15', () => {
         expect(sum([7, 8])).toBe(15)
     })
